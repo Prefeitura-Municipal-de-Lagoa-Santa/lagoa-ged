d1e38774b803c55f084679d4d6d0ad6ccd17a94a
LeandroLDomingos <leandrolucasdomingos15@outlook.com>
2025-08-11 15:22:44 +0000

    feat(docker): utilizar variáveis de ambiente para credenciais do MongoDB e Redis no docker-compose

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+            // Força o uso de HTTPS em URLs geradas pelo Laravel
+            URL::forceScheme('https');
+        }
     }
 }
